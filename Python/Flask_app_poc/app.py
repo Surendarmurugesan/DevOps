e99e3afb4f7cdcfd45cbaf35abef7286b87e14fa
Surendar <surendarthiyagu96@gmail.com>
2021-12-24 15:58:20 +0530

    Python basic programming scripts

--- Python/Flask_app_poc/app.py
+++ Python/Flask_app_poc/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, abort
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -9,3 +10,16 @@ def hello():
 @app.route('/about/')
 def about():
     return '<h3>This is a Flask web application.</h3>'
+
+@app.route('/capitalize/<word>/')
+def capitalize(word):
+    return '<h1>{}</h1>'.format(escape(word.capitalize()))
+
+## Debugging A Flask Application
+@app.route('/users/<int:user_id>/')
+def user(user_id):
+    users = ['Dhanush', 'sergio', 'surendar']
+    try:
+        return '<h2>Helloo {}</h2>'.format(users[user_id])
+    except IndexError:
+        abort(404)
